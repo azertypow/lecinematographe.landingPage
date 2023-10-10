2ac227f6cd6a500c9e9db9d2ae50410d2456f8c3
azertypow <azertypow@gmail.com>
2023-10-10 08:40:28 +0200

    update content

--- src/App.vue
+++ src/App.vue
@@ -53,9 +53,24 @@
                             <div
                                 class="v-app__info__slot__content"
                             >
-                                <p>ARTICLE DU 24 HEURES
-                                    <br><a href="">LIRE L’ARTICLE </a>
+                                <p>Stéphanie Arboit, «&nbsp;Le Cinématographe va projeter un nouveau chapitre de sa vie&nbsp;», 24 Heures, 11 juillet 2023
+                                    <br><a href="https://www.24heures.ch/le-cinematographe-va-projeter-un-nouveau-chapitre-de-sa-vie-466711780457">LIRE L’ARTICLE </a>
                                 </p>
+                                <img
+                                    class="v-app__info__container__img"
+                                    alt="article cover"
+                                    src="./assets/presse_24_heures_cinematographe.jpeg"
+                                >
+                            </div>
+
+                            <div
+                                class="v-app__info__slot__content"
+                            >
+                                <p>
+                                    Alexandre Ducommun, « Un nouveau départ pour le Cinématographe », Cinébulletin, 11 juillet 2023
+                                    <br><a href="https://cinebulletin.ch/fr_CH/news/un-nouveau-depart-pour-le-cinematographe">LIRE L’ARTICLE </a>
+                                </p>
+
                                 <img
                                     class="v-app__info__container__img"
                                     alt="article cover"
@@ -79,48 +94,18 @@
                     <h4>Le Cinématographe</h4>
 
                     <p>
-                        Ouvert en 1981, le Cinématographe est une salle de cinéma de 102 places située au
-                        Casino de Montbenon, au cœur de Lausanne. Propriété de la Ville de Lausanne, son exploitation et
-                        sa
-                        programmation sont conduites jusqu’à aujourd’hui par la Cinémathèque Suisse. Cette dernière
-                        cessera
-                        ses projections au Cinématographe à la fin de l’année 2023 pour déplacer ses activités publiques
-                        au
-                        cinéma Capitole rénové. À partir de 2024, la salle du Cinématographe sera gérée par une nouvelle
-                        équipe.
+                        Ouvert en 1981, le Cinématographe est une salle de cinéma de 102 places située au Casino de Montbenon, au cœur de Lausanne. Propriété de la Ville de Lausanne, son exploitation et sa programmation sont conduites jusqu’à aujourd’hui par la Cinémathèque Suisse. Cette dernière cessera ses projections au Cinématographe à la fin de l’année 2023 en vue de déplacer ses activités publiques au cinéma Capitole rénové. À partir de 2024, la salle du Cinématographe sera gérée par une nouvelle équipe.
+                    </p>
+                    <p>
+                        Sélectionné par la Ville de Lausanne à la suite d’un appel à projets, le Collectif du nouveau Cinématographe assurera la gestion, la programmation et l’exploitation de la salle dès mars 2024. Au centre de notre projet réside l’envie de partager avec tous les publics un cinéma de qualité en dialogue avec des enjeux de société contemporains. À travers un large choix de sorties et de reprises, nous ferons la part belle aux nouveautés et valoriserons un spectre varié de films, réalisés tant par des cinéastes émergent·e·x·s que reconnu·e·x·s. La programmation du Cinématographe offrira également des cycles, des événements, une programmation destinée au jeune public, des actions de médiation et de nombreuses collaborations avec des festivals et des ciné-clubs.
+                    </p>
+                    <p>
+                        Durant l’automne 2023 et l’hiver 2024, le Collectif du nouveau Cinématographe organise divers événements afin de rencontrer les publics actuels et futurs de la salle. Pour mener à bien notre projet, nous comptons sur différents soutiens. Une collecte de fonds est lancée le 15 octobre 2023 pour répondre à nos besoins liés aux frais de reprise. Chaque contribution nous aidera à subvenir aux coûts de rénovation de la salle, à améliorer les espaces d'accueil et à créer une buvette conviviale.
+                    </p>
+                    <p>
+                        Nous souhaitons faire du Cinématographe un lieu central de la vie culturelle lausannoise défendant une culture inclusive et le plaisir du cinéma en salle. Rejoignez-nous dans cette nouvelle aventure !
                     </p>
 
-                    <p>
-                        Sélectionné par la Ville de Lausanne à la suite d’un appel à projets, le Collectif du
-                        nouveau Cinématographe assurera la gestion, la programmation et l’exploitation de la salle dès
-                        mars
-                        2024. Au centre de notre projet réside l’envie de partager avec tous les publics un cinéma de
-                        qualité en dialogue avec des enjeux de société contemporains. À travers un large choix de
-                        sorties et de reprises, nous ferons la part belle aux nouveautés et valoriserons un spectre
-                        varié
-                        de films, réalisés tant par des cinéastes émergent·e·x·s que reconnu·e·x·s. La programmation
-                        du
-                        Cinématographe proposera également des cycles, des événements, une programmation destinée au
-                        jeune
-                        public, des actions de médiation et de nombreuses collaborations avec des festivals et des
-                        ciné-clubs.
-                    </p>
-
-                    <p>
-                        Durant l’automne 2023 et l’hiver 2024 le Collectif du nouveau Cinématographe organise
-                        divers événements, afin de rencontrer les publics actuels et futurs de la salle. Pour mener à
-                        bien
-                        ce projet, nous comptons sur différents soutiens. Une collecte de fonds est lancée le 15 octobre
-                        2023 pour répondre à nos besoins liés aux frais de reprise. Chaque contribution nous aidera à
-                        subvenir aux coûts de rénovation de la salle, à améliorer les espaces d’accueil et à créer une
-                        buvette conviviale.
-                    </p>
-
-                    <p>
-                        Nous souhaitons faire du Cinématographe un lieu central de la vie culturelle
-                        lausannoise défendant une culture inclusive et le plaisir du cinéma en salle. Rejoignez-nous
-                        dans cette nouvelle&nbsp;aventure&nbsp;!
-                    </p>
 
                     <h6>
                         Nota bene Si vous cherchez le programme actuel du Cinématographe, rendez&#x2011;vous sur le site
@@ -191,6 +176,10 @@ import AppFooter from "./components/AppFooter.vue";
             border-bottom: solid 1px white;
             padding-top: 1rem;
             padding-bottom: 1rem;
+
+            > p {
+                max-width: 25rem;
+            }
         }
     }
 
